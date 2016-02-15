da4622904efc9853fb467edd5fad4526ba37c587
sklee <sklee2@me.com>
2016-02-15 15:08:33 +0900

    edit

--- src/main/java/com/hyulib/Db2fileApplication.java
+++ src/main/java/com/hyulib/Db2fileApplication.java
@@ -24,30 +24,38 @@ public class Db2fileApplication implements CommandLineRunner{
 	}
 	@Override
 	public void run(String... args) throws Exception {
-		StringBuilder sb = new StringBuilder();
-		for (String option : args) {
-			sb.append(" ").append(option);
-		}
-		sb = sb.length() == 0 ? sb.append("No Options Specified") : sb;
-		System.out.println(sb);
-		logger.info(String.format("WAR launched with following options: %s", sb.toString()));
-		System.out.println(userInfoService.getUserInfo("000A011982"));
-		for (String option : args) {
-			System.out.println(option);
-		}
-		List users = userInfoService.getFromToUserInfo(args[0], args[1]);
-		for (Object user : users) {
-			System.out.println(user);
-		}
-		for (Object userIdName : users) {
-			System.out.print(((UserInfo)userIdName).getUserId() + " ");
-			System.out.println(((UserInfo) userIdName).getUserName() + " ");
-		}
+        if (args.length == 0)
+        {
+            logger.info("No arguments!");
+            System.exit(0);
+        } else {
+            logger.info("OK! Two arguments.");
 
-		for (Object user2 : users) {
-			String userId= ((UserInfo)user2).getUserId();
-			String userInfo = user2.toString();
-			makeFile(userId,userInfo);
+
+    //		StringBuilder sb = new StringBuilder();
+    //		for (String option : args) {
+    //			sb.append(" ").append(option);
+    //		}
+    //		sb = sb.length() == 0 ? sb.append("No Options Specified") : sb;
+    //		System.out.println(sb);
+    //		logger.info(String.format("WAR launched with following options: %s", sb.toString()));
+
+            for (String option : args) {
+                System.out.println(option);
+            }
+            List<UserInfo> users = userInfoService.getFromToUserInfo(args[0], args[1]);
+
+            users.forEach(System.out::println);
+            for (Object userIdName : users) {
+                System.out.print(((UserInfo)userIdName).getUserId() + " ");
+                System.out.println(((UserInfo) userIdName).getUserName() );
+            }
+
+            for (Object user2 : users) {
+                String fileName= ((UserInfo)user2).getUserId();
+                String userInfoString = user2.toString();
+                makeFile(fileName,userInfoString);
+            }
 		}
 	}
 
